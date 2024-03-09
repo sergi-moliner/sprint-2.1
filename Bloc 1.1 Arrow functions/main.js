930cbf00bbefb19ec469213950ed3315b01deb44
sergi.moliner <sergilopezmoliner@gmail.com>
2024-03-09 14:18:07 +0100

    bloc1-ex-4-5

--- Bloc 1.1 Arrow functions/main.js
+++ Bloc 1.1 Arrow functions/main.js
@@ -21,3 +21,24 @@ class Person{
 
 const persona = new Person("Manolito");
 persona.greet();
+
+/* Exercici 4 */
+
+const printNumbers = (arrayNumeros) => {
+    let i = 0;
+    while (i < arrayNumeros.length) {
+        console.log(arrayNumeros[i]);
+        i++;
+    }
+}
+
+const numbers = [1, 2, 3, 4, 5, 6];
+printNumbers(numbers);
+
+/* Exercici 5 */
+function saludar(){
+    console.log('Hola, despues de 3 segundos');
+}
+
+const saludo = () => setTimeout(saludar, 3000);
+saludo();
