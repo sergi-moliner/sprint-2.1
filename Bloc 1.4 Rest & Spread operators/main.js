354c675072dc61425766ca1da2e4c621a5c83dcb
sergi.moliner <sergilopezmoliner@gmail.com>
2024-03-09 17:51:04 +0100

    bloc4-ex-4-5

--- Bloc 1.4 Rest & Spread operators/main.js
+++ Bloc 1.4 Rest & Spread operators/main.js
@@ -21,3 +21,29 @@ function suma (...numeros){
 let nums = [8, 3, 2, 7];
 let resultado = suma(...nums);
 console.log(resultado);
+
+/* Exercici 3 */
+
+const objecte1 = {
+    mensaje1: 'primera cosa',
+    mensaje2: 'segunda cosa'
+};
+
+const objecte2 = { ...objecte1 };
+objecte2.mensaje1 = "cosa nueva";
+
+console.log("objecte1:", objecte1);
+console.log("objecte2:", objecte2);
+
+/* Exercici 4 */
+
+const arrayCosas = ['tostada', 'mantequilla', 14, '85-20', 1, 'mermelada'];
+
+const [primeraCosa, segundaCosa, , , , ] = arrayCosas;
+const [ , , ...restoArray] = arrayCosas;
+
+const var1 = [primeraCosa, segundaCosa];
+const var2 = restoArray;
+
+console.log(var1);
+console.log(var2);
