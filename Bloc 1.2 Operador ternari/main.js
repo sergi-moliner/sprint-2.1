4aaa2906b56ccc86fa6d8dd23c14c9fea72da7e5
sergi.moliner <sergilopezmoliner@gmail.com>
2024-03-09 14:22:46 +0100

    bloc2-ex-3-4

--- Bloc 1.2 Operador ternari/main.js
+++ Bloc 1.2 Operador ternari/main.js
@@ -13,3 +13,28 @@ let num2 = 14;
 const numMesGran = num1 > num2 ? num1 : num2;
 
 console.log(`El ${numMesGran} es més gran.`);
+
+/* Exercici 3 */
+
+const trobarMax = (a, b, c) => (a > b) ? (a > c ? a : c) : (b > c ? b : c);
+
+const numeros = trobarMax(4, 8, 3);
+
+console.log(numeros);
+
+/* Exercici 4 */
+
+let pares = [];
+let impares = [];
+
+function parOimpar(array){
+    for (nb of array){
+        (nb % 2 == 0) ? pares.push(nb) : impares.push(nb);
+    }
+}
+
+const array = [1, 2, 3, 4, 5, 6, 7, 8];
+parOimpar(array);
+
+console.log(`Numeros pares: ${pares}.`);
+console.log(`Numeros impares: ${impares}.`);
