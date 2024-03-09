dee0044b0323bab73eedd49fb1654b159face470
sergi.moliner <sergilopezmoliner@gmail.com>
2024-03-09 18:17:21 +0100

    bloc7-ex-3-5

--- Bloc 1.7 Promises & Async/main.js
+++ Bloc 1.7 Promises & Async/main.js
@@ -22,3 +22,49 @@ promesa()
     .catch(error => {
         console.error(error.message);
     });
+
+/* Exercici 3 */
+
+let input = 'Holo'
+
+function promiseReject() {
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            if (input == 'Hola')
+                resolve('"Hola" bien escrito')
+            else{
+                reject(new Error('No hay "Hola".'))
+            }
+        }, 2000);
+    });
+}
+
+promiseReject()
+    .then((resultado) => {
+        console.log('Success:', resultado)
+    })
+    .catch((razon) => {
+        console.error('Error:', razon.message);
+    });
+
+/* Exercici 4 */
+
+async function asyncFunction(){
+
+    const resultat = await promesa();
+    console.log('Async result:', resultat);
+}
+
+/* Exercici 5 */
+
+async function asyncFunction(){
+
+    try {
+        const resultat = await promesa();
+        console.log('Async result:', resultat);
+    } catch (error) {
+        console.error('Error:', error.message)
+    }
+}
+
+asyncFunction();
