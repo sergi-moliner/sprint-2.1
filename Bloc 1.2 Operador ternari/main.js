2a186bd7c5eb27c2649c857b0841e99731214a5f
sergi.moliner <sergilopezmoliner@gmail.com>
2024-03-09 14:21:46 +0100

    bloc2-ex-1-2

---
+++ Bloc 1.2 Operador ternari/main.js
@@ -0,0 +1,15 @@
+/* Exercici 1 */
+
+let edat = 17;
+const potConduir = edat >= 18 ? 'Pots conduir' : 'No pots conduir';
+
+console.log(potConduir);
+
+/* Exercici 2 */
+
+let num1 = 18;
+let num2 = 14;
+
+const numMesGran = num1 > num2 ? num1 : num2;
+
+console.log(`El ${numMesGran} es més gran.`);
